03a4fe5f6636eb487b9efccca0b12151edc8491e
ed.gokmen <ed.gokmen@auros.global>
2025-01-06 17:56:15 +0000

    added places to get the treesitter grammars from

--- init.el
+++ init.el
@@ -270,6 +270,25 @@ The DWIM behaviour of this command is as follows:
   ;; Auto parenthesis matching
   ((prog-mode . electric-pair-mode)))
 
+;; Needed to get a list of places to get treesitter grammars from.
+;; Use nix to get these in one shot where possible, this is only a fallback
+(setq treesit-language-source-alist
+   '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+     (cmake "https://github.com/uyha/tree-sitter-cmake")
+     (css "https://github.com/tree-sitter/tree-sitter-css")
+     (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+     (go "https://github.com/tree-sitter/tree-sitter-go")
+     (html "https://github.com/tree-sitter/tree-sitter-html")
+     (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+     (json "https://github.com/tree-sitter/tree-sitter-json")
+     (make "https://github.com/alemuller/tree-sitter-make")
+     (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+     (python "https://github.com/tree-sitter/tree-sitter-python")
+     (toml "https://github.com/tree-sitter/tree-sitter-toml")
+     (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+     (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
 (use-package markdown-mode
   :hook ((markdown-mode . visual-line-mode)))
 
